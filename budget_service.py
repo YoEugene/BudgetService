564a4df9ac6191f29374839b618a2d303b571b70
Eugene Hsu <yoeugene.hsu@gmail.com>
2022-10-08 23:31:27 +0800

    refactor: remove useless staticmethod

--- budget_service.py
+++ budget_service.py
@@ -12,17 +12,14 @@ class BudgetService:
         self.budgets = []
 
     def query(self, start: datetime, end: datetime) -> Decimal:
-        # get data from db
-        self.budgets = self.__get_budgets()
+        self.budgets = BudgetsInterface.get_all()
 
-        if start > end:  # deal with illegal input date range
+        if start > end:
             return Decimal(0)
 
         month_delta = (end.year - start.year) * 12 + (end.month - start.month)
-        # within same month
         if month_delta == 0:
             return self.__get_budget_within_same_month(start, end)
-        # across different month
         else:
             return self.__get_budget_across_different_months(start, end, month_delta)
 
@@ -30,24 +27,16 @@ class BudgetService:
         return self.__get_partial_month_budget(start, end)
 
     def __get_budget_across_different_months(self, start, end, month_delta) -> Decimal:
-        total_budget = 0
-        total_budget += self.__get_budget_with_start_date(start)
+        total_budget = Decimal(0)
+        total_budget += self.__get_partial_month_budget_with_start(start)
         for i in range(1, month_delta):
             total_budget += self.__get_entire_month_budget(
                 start + relativedelta(months=i)
             )
-        total_budget += self.__get_budget_with_end_date(end)
+        total_budget += self.__get_partial_month_budget_with_end(end)
         return total_budget
 
-    @staticmethod
-    def __get_budgets() -> List[Budget]:
-        return BudgetsInterface.get_all()
-
-    @staticmethod
-    def __calculate_budget(days: int, days_of_month: int, month_budget: int) -> float:
-        return month_budget / days_of_month * days
-
-    def __get_budget_with_start_date(self, start: datetime) -> Decimal:
+    def __get_partial_month_budget_with_start(self, start: datetime) -> Decimal:
         end = (
             datetime(start.year, start.month, 1)
             + relativedelta(months=1)
@@ -55,7 +44,7 @@ class BudgetService:
         )
         return self.__get_partial_month_budget(start, end)
 
-    def __get_budget_with_end_date(self, end: datetime) -> Decimal:
+    def __get_partial_month_budget_with_end(self, end: datetime) -> Decimal:
         start = datetime(end.year, end.month, 1)
         return self.__get_partial_month_budget(start, end)
 
@@ -71,3 +60,7 @@ class BudgetService:
             if i.year_month == date.strftime("%Y%m"):
                 return i.amount
         return 0
+
+    @staticmethod
+    def __calculate_budget(days: int, days_of_month: int, month_budget: int) -> float:
+        return month_budget / days_of_month * days
