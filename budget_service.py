40edb97c1a60e77d0c00c97a3ad3d3fd3b4282cc
Eugene Hsu <yoeugene.hsu@gmail.com>
2022-10-12 17:47:00 +0800

    refactor: use same algorithm for end month budget calculate

--- budget_service.py
+++ budget_service.py
@@ -54,7 +54,7 @@ class BudgetService:
                     total_budget += budget.daily_amount() * days
                 elif budget.yearMonth == end.strftime("%Y%m"):
                     day = end.day
-                    total_budget += budget.amount / days_of_month * day
+                    total_budget += budget.daily_amount() * day
                 else:
                     total_budget += budget.amount
                 current = current + relativedelta(months=+1)
